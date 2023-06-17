62595ec3e5800e4689226cf585fe90021016da5a
syeddanyalhassan <syeddanyalhassan@gmail.com>
2023-06-17 19:38:17 +0500

    new files added

--- src/main/java/com.qa.rta/pages/HomePage.java
+++ src/main/java/com.qa.rta/pages/HomePage.java
@@ -29,8 +29,7 @@ public class HomePage extends BasePage
         return driver.getTitle();
     }
     public void secondSectionClick()  {
-        WebDriverWait wait=new WebDriverWait(driver,120);
-        wait.until(ExpectedConditions.visibilityOf(secondSection));
+         
         secondSection.click();
     }
 
